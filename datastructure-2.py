bfeb8751a3f06de4e4e16099a0a160792d677ab2
rajeevjfardev <rajeevjfardev@gmail.com>
2024-03-03 17:54:57 -0600

    datastructure2

--- datastructure-2.py
+++ datastructure-2.py
@@ -1,11 +1,8 @@
 numbers = [1, 2, 3, 4, 5]
 
-# Initialize a variable to store the product with a starting value of 1 (since 1 is the multiplicative identity)
 product = 1
 
-# Loop through each number in the list and multiply it to the product variable
 for number in numbers:
     product *= number
 
-# Print the total product
 print("The product of all the items in the list is:", product)
